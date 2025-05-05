2dd9a59aba692beabbeba2b067ddb3ab63789b6e
AvneetKaur <avneetu26@gmail.com>
2025-05-05 17:36:32 -0400

    more components, login, redux

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 # added tailwind and daisy ui
 # navbar component
 # react-router-dom added
+# login page
+# add axios
+# always pass withCredentials (to make sure cookies are also used)
+# add redux
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+
+  const user = useSelector(store => store.user);
+
   return (
     <div>
       <div className="navbar bg-base-100 shadow-sm">
@@ -9,14 +13,15 @@ const Navbar = () => {
         </div>
         <div className="flex gap-2">
           
-          <div className="dropdown dropdown-end mx-4">
+          { user && <div className="dropdown dropdown-end mx-4">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
                 <img
                   alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                  src={user.photoUrl}/>
               </div>
             </div>
+          
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
@@ -30,6 +35,7 @@ const Navbar = () => {
               <li><a>Logout</a></li>
             </ul>
           </div>
+          }
         </div>
       </div>
     </div>
